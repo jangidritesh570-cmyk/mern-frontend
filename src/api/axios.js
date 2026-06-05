13d31b7f605a9d7df5319e32e0445e114ebe1614
jangidritesh570-cmyk <jangidritesh570@gmail.com>
2026-06-06 00:29:10 +0530

    updated

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://student-management-system-9ypu.onrender.com/api/v1",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
